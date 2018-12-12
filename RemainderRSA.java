8a3ab08d498640022a6002870c95ee5d6aec430b
smitsd0 <43766738+smitsd0@users.noreply.github.com>
2018-12-12 14:01:10 -0500

    Add files via upload

--- RemainderRSA.java
+++ RemainderRSA.java
@@ -3,32 +3,23 @@ package rsa;
 import java.util.Random;
 
 public class RemainderRSA {
-	
+
 	public static long randPrime(int m, int n, Random rand) {
-		int i = 0;
-		int num = 0;
-		String primes = "";
-		
-		for(i = m; i <= n; i++){
-			int counter = 0;
-			for(num = i; num >= 1; num--) {
-				if(i % num == 0){
-					counter = counter + 1;
-				}
-			}
-			if(counter == 2) {
-				primes = primes + i + " ";
-			}
+		long num = rand.nextInt(((n - m) + 1) + m);
+		boolean isPrime = false;
+		while(isPrime == false) {
+		    int counter = 0;
+		    long i = 0;
+		    for(i = num; i >= 1; i--){
+		    	if(num%i == 0)
+		    		counter = counter + 1;
+		    }
+		    if(counter == 2)
+		    	isPrime = true;
 		}
-		long value = rand.nextLong();
-		return value;
-		
+		return num;
 	}
 	
-	public static long relPrime(long n, Random rand) {
-		if(rand == 0)
-			return n;
-		return relPrime(rand, n%rand);
-	}
+//	public static long relPrime(long n, Random rand) {	}
 	
 }
